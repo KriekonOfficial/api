97065b55cf66a1651776771d06d1f4d8e9a0ca5c
V. Marone <xnjxdanger0us@gmail.com>
2020-04-21 03:59:01 -0400

    Update test file

--- src/Controllers/Root/Test/Index.php
+++ src/Controllers/Root/Test/Index.php
@@ -6,9 +6,25 @@ class Index extends \Core\Controller
 {
 	public function index()
 	{
-		$object = new \Modules\Account\Account();
-		\Core\Response\Dump::var($object->find(2));
-		//\Core\Response\Dump::var($object);
+		/*$object = new \Modules\Account\Account();
+		$model = $object->find(1);
+
+		\Core\Response\Dump::var($model->createEntity());*/
+
+		$model = new \Modules\Account\Models\AccountModel();
+		$model->setEmail('vinnie.marone35@gmail.com');
+		$model->setFirstName('Vinnie');
+		$model->setLastName('Marone');
+		$model->setDateOfBirth('1996-04-12');
+		$model->setRegistrationTime(date(DATEFORMAT_STANDARD));
+		$model->setPasswordHash(password_hash('password1234', PASSWORD_BCRYPT, array('cost' => 12)));
+
+		$validator = new \Modules\Account\Models\AccountValidator($model);
+		$validator->addValidator('validateEmail');
+		$validator->addRule('validateEmail', ['email']);
+		$validator->validate();
+
+		\Core\Response\Dump::var($validator);
 
 		return new \Core\Response\ErrorResponse(500, 'test1234');
 	}
