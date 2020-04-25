7b13760d2c22e945a49bde5177bf1e7533082361
V. Marone <xnjxdanger0us@gmail.com>
2020-04-25 00:21:46 -0400

    BaseValidator minLength function add

--- src/Core/Model/BaseValidator.php
+++ src/Core/Model/BaseValidator.php
@@ -4,7 +4,11 @@ namespace Core\Model;
 
 trait BaseValidator
 {
-	public function minLength($value, $min)
+	/**
+	* @return true on length greater than min|false on failure
+	*/
+	public function minLength($value, $min) : bool
 	{
+		return strlen($value) > $min;
 	}
 }
