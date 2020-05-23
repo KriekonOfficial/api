1cffbc4fe528e1819acbda4e64e8af156b3f2253
V. Marone <xnjxdanger0us@gmail.com>
2020-05-23 18:05:49 -0400

    Move autoload outside of Application::run

--- api.kriekon.com/index.php
+++ api.kriekon.com/index.php
@@ -7,6 +7,8 @@ require(ROOT_PATH . '/src/includes/global_constants.php');
 require(ROOT_PATH . '/src/includes/global_functions.php');
 require(ROOT_PATH . '/src/Core/Application.php');
 
+Application::autoload();
+
 Application::shutdown_handler();
 
 Application::exception_handler();
