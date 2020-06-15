c6dd4656f0a2a772cddf43255e7a9fbf7997bf48
V. Marone <vinnie.marone35@gmail.com>
2020-06-15 04:09:34 -0400

    Functional status list endpoint

--- src/Controllers/V1/User/Status.php
+++ src/Controllers/V1/User/Status.php
@@ -7,12 +7,27 @@ use Core\Request\Request;
 use Core\Response\ErrorResponse;
 use Core\Response\SuccessResponse;
 
+use Modules\Status\StatusGateway;
+
 class Status extends Controller
 {
 	public function listStatus(Request $request, array $get_params)
 	{
-		//get_params will be pagination options.
-		return new ErrorResponse(200, 'Test', $get_params);
+		$page = $get_params['page'] ?? 1;
+		$per_page = $get_params['per_page'] ?? 25;
+
+		$status = new StatusGateway();
+		$list = $status->listStatus($request->getAuth()->getAccount()->getACCTID(), $page, $per_page);
+
+		if ($status->hasError())
+		{
+			return new ErrorResponse(400, $status->getErrors());
+		}
+
+		return new SuccessResponse(200, [
+			'page' => $page,
+			'threads' => $list
+		]);
 	}
 
 	public function createStatus(Request $request)
@@ -33,5 +48,5 @@ class Status extends Controller
 	public function deleteStatus(Request $request, int $status_id)
 	{
 
-}
+	}
 }
