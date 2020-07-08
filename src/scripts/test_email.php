204388027f3fc4b326ac6e4106301a77e4015fe9
V. Marone <vinnie.marone35@gmail.com>
2020-07-08 03:53:56 -0400

    test_email further debug

--- src/scripts/test_email.php
+++ src/scripts/test_email.php
@@ -3,6 +3,39 @@
 require('../Core/Application.php');
 Application::autoload();
 
-$mail = new MailWrapper('noreply@kriekon.com', EMAILS['noreply@kriekon.com'], 'mail.kriekon.com', true);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+/*$mail = new MailWrapper('noreply@kriekon.com', EMAILS['noreply@kriekon.com'], 'mail.kriekon.com', true);
 $mail->addAddress('xnjxdanger0us@gmail.com', SITE_NAME);
-$mail->send('test', 'hello world', true);
+$mail->send('test', 'hello world', true);*/
+
+$username = 'noreply@kriekon.com';
+$password = EMAILS['noreply@kriekon.com'];
+
+$mail = new PHPMailer(true);
+$mail->SMTPDebug = SMTP::DEBUG_LOWLEVEL;
+
+$mail->isSMTP();
+$mail->Host = $host;
+$mail->SMTPAuth = true;
+$mail->Username = $username;
+$mail->Password = $password;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->SMTPOptions = [
+'ssl' => array(
+	'verify_peer' => false,
+	'verify_peer_name' => false,
+	'allow_self_signed' => true
+)];
+
+$mail->setFrom($username);
+
+$mail->addAddress('xnjxdanger0us@gmail.com');
+
+$mail->Subject = 'test1234';
+$mail->Body = 'mail from prod';
+
+$mail->send();
