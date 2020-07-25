ba6ce34f9627d0bc6400d1ca239968bd09a978fd
V. Marone <vinnie.marone35@gmail.com>
2020-07-25 18:55:41 -0400

    htmlEncode if it's ever going to be needed.

--- src/includes/global_functions.php
+++ src/includes/global_functions.php
@@ -22,3 +22,8 @@ function humanReadable(string $string, string $delimiter = '_') : string
 {
 	return ucwords(str_replace($delimiter, ' ', $string));
 }
+
+function htmlEncode(string $string) : string
+{
+	return htmlspecialchars($string, ENT_QUOTES);
+}
