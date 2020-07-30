c8b9f70da63e65ae4ef1dfc6ce6807ae18e9f9e6
V. Marone <vinnie.marone35@gmail.com>
2020-07-30 06:41:47 -0400

    Fix KeyGenerator

--- src/Core/Util/KeyGenerator.php
+++ src/Core/Util/KeyGenerator.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace Modules\Password;
+namespace Core\Util;
 
 class KeyGenerator
 {
 	public static function generateToken(int $length = 12) : string
 	{
-		return md5(bin2hex(random_bytes($length)));
+		return substr(bin2hex(random_bytes($length)), 0, $length);
+	}
+
+	public static function generateMD5Token(int $iteration = 45) : string
+	{
+		return md5(bin2hex(random_bytes($iteration)));
 	}
 }
