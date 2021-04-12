776d47cdf92f0da427431f8dc6125f7a12ddb190
Vinnie marone <vinnie.marone35@gmail.com>
2021-04-12 01:35:48 -0400

    Lets start adding tests.

---
+++ tests/StatusTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+require(__DIR__.'/../src/includes/bootstrap_script.php');
+
+use PHPUnit\Framework\TestCase;
+use Modules\Status\StatusGateway;
+use Modules\Status\Models\StatusModel;
+
+class StatusTest extends TestCase
+{
+	public function testGetStatus()
+	{
+
+	}
+}
